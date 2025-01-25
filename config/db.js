08c337026fbafac4378ce43bdfbad7c22e69c034
Ayden <prohaddockal8@gmail.com>
2025-01-25 08:06:55 -0600

    Fix MongoDB connection with environment variable

--- config/db.js
+++ config/db.js
@@ -8,8 +8,8 @@ const connectDB = async () => {
         });
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
-        console.error(`Error: ${error.message}`);
-        process.exit(1); // Exit process with failure
+        console.error(`Error connecting to MongoDB: ${error.message}`);
+        process.exit(1); // Exit the process with failure
     }
 };
 
