d55cb6e2adf56f3d568ccc85f896aae288116c20
Ayden <prohaddockal8@gmail.com>
2025-01-26 17:59:49 -0600

    fix1

--- config/db.js
+++ config/db.js
@@ -1,13 +1,14 @@
 const mongoose = require('mongoose');
-
 const MONGO_URI = process.env.MONGO_URI;
 
-// Connect to the MongoDB databases
-const connectDB = () => {
-  mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('MongoDB connected'))
-    .catch(err => console.error('MongoDB connection error:', err));
-};
+const notesConnection = mongoose.createConnection(`${MONGO_URI}Notes`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 
-// Export the function so it can be used elsewhere
-module.exports = connectDB;
+const historyConnection = mongoose.createConnection(`${MONGO_URI}History`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+module.exports = { notesConnection, historyConnection };
--- models/Note.js
+++ models/Note.js
@@ -1,10 +1,12 @@
+const { notesConnection } = require('../config/db');  // Ensure the path is correct
 const mongoose = require('mongoose');
-const { notesConnection } = require('../config/db');
 
 const NoteSchema = new mongoose.Schema({
     title: { type: String, required: true },
     MT: { type: Number, required: true },
     TV: { type: Number, required: true },
 }, { timestamps: true, collection: 'Notes' });
-
+if (!notesConnection) {
+    throw new Error('notesConnection is undefined');
+}
 module.exports = notesConnection.model('Note', NoteSchema);
