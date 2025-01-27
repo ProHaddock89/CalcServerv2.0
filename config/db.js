7e5defe5de0fbd8f97fdd7beef5a73c8c4dcf03b
Ayden <prohaddockal8@gmail.com>
2025-01-26 18:04:00 -0600

    fix1

--- config/db.js
+++ config/db.js
@@ -1,6 +1,19 @@
 const mongoose = require('mongoose');
 const MONGO_URI = process.env.MONGO_URI;
 
+const connectDB = async () => {
+    try {
+        await mongoose.connect(MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('MongoDB Connected...');
+    } catch (err) {
+        console.error(err.message);
+        process.exit(1); // Exit process with failure
+    }
+};
+
 const notesConnection = mongoose.createConnection(`${MONGO_URI}Notes`, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -11,4 +24,8 @@ const historyConnection = mongoose.createConnection(`${MONGO_URI}History`, {
     useUnifiedTopology: true,
 });
 
-module.exports = { notesConnection, historyConnection };
+module.exports = { connectDB, notesConnection, historyConnection };
+
+
+module.exports = { connectDB, notesConnection, historyConnection };
+
