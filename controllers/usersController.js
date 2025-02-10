5d2322dd57fa36eb329c88cdd5ac589a883384aa
Ayden <prohaddockal8@gmail.com>
2025-02-09 19:00:37 -0600

    .create fix

---
+++ controllers/usersController.js
@@ -0,0 +1,27 @@
+const bcrypt = require("bcryptjs");
+const User = require("../models/User");
+
+const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    // Check if user exists
+    let user = await User.findOne({ email });
+    if (user) return res.status(400).json({ message: "User already exists" });
+
+    // Hash password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create and save user
+    user = new User({ name, email, password: hashedPassword });
+    await user.save();
+
+    res.status(201).json({ message: "User registered successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+// Make sure to export as an object
+module.exports = { registerUser };
