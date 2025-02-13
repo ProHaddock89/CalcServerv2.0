09e9f780e383577fae2218b4a360193bae9f262c
Ayden <prohaddockal8@gmail.com>
2025-02-13 17:14:58 -0600

    notesaving

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,4 +1,5 @@
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 
 const registerUser = async (req, res) => {
@@ -16,12 +17,14 @@ const registerUser = async (req, res) => {
     user = new User({ name, email, password: hashedPassword });
     await user.save();
 
-    res.status(201).json({ message: "User registered successfully" });
+    // ✅ Generate token
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "1h" });
+
+    res.status(201).json({ message: "User registered successfully", token, user });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Server error" });
   }
 };
 
-// Make sure to export as an object
 module.exports = { registerUser };
