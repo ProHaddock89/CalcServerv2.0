b79ec1804978e6a74edf9b1f387e824add671f6b
Ayden <prohaddockal8@gmail.com>
2025-07-23 13:22:29 -0500

    changePass

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -30,5 +30,25 @@ const registerUser = async (req, res) => {
     res.status(500).json({ message: "Server error" });
   }
 };
+const changePassword = async (req, res) => {
+  try {
+    const userId = req.user.id;
+    const { currentPassword, newPassword } = req.body;
 
-module.exports = { registerUser };
+    const user = await User.findById(userId);
+    const isMatch = await bcrypt.compare(currentPassword, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Current password is incorrect" });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(newPassword, salt);
+    await user.save();
+
+    res.status(200).json({ message: "Password updated successfully" });
+  } catch (err) {
+    console.error("Password change error:", err);
+    res.status(500).json({ message: "Something went wrong" });
+  }
+};
+module.exports = { registerUser, changePassword };
